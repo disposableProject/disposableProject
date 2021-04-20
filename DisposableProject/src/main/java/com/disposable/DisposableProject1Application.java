b9230560889ee3b59c3e7fdd0b28c9be4e5fd277
USER <USER@DESKTOP-HHBG4TS>
2021-04-20 16:43:32 +0900

    210420 1642 commit
    mapper 연결 설정 추가

--- DisposableProject/src/main/java/com/disposable/DisposableProject1Application.java
+++ DisposableProject/src/main/java/com/disposable/DisposableProject1Application.java
@@ -1,7 +1,15 @@
 package com.disposable;
 
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 @SpringBootApplication
 public class DisposableProject1Application {
@@ -9,5 +17,14 @@ public class DisposableProject1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(DisposableProject1Application.class, args);
 	}
-
+	@Bean
+	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+		SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+		sessionFactory.setDataSource(dataSource);
+		
+		Resource[] res = new PathMatchingResourcePatternResolver().getResources("classpath:com/disposable/**/mapper/*.xml");
+		sessionFactory.setMapperLocations(res);
+		
+		return sessionFactory.getObject();
+	}
 }
