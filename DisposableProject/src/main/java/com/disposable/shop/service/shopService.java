162ef18099f811e025bf6114c359c3ac944f6e23
sonhoil <48711247+sonhoil@users.noreply.github.com>
2021-07-06 10:41:05 +0900

    Add files via upload

--- DisposableProject/src/main/java/com/disposable/shop/service/shopService.java
+++ DisposableProject/src/main/java/com/disposable/shop/service/shopService.java
@@ -1,5 +1,9 @@
 package com.disposable.shop.service;
 
-public interface shopService {
+import java.util.List;
+import java.util.Map;
 
+public interface shopService {
+	public List<Map<String,Object>> shopMain(String shopnum);
+	public List<Map<String,Object>> getFoodOption(Map<String,Object> paramMap);
 }
--- DisposableProject/src/main/java/com/disposable/shop/service/shopServiceImpl.java
+++ DisposableProject/src/main/java/com/disposable/shop/service/shopServiceImpl.java
@@ -1,5 +1,30 @@
 package com.disposable.shop.service;
 
-public class shopServiceImpl {
+import java.util.List;
+import java.util.Map;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.disposable.management.service.managementService;
+import com.disposable.shop.dao.shopDao;
+
+@Service("shopService")
+@MapperScan(basePackages="com.disposable.shop.dao")
+public class shopServiceImpl implements shopService{
+	
+	@Autowired
+	private shopDao shopDao;
+	
+	@Override
+	public List<Map<String, Object>> shopMain(String shopnum) {
+		// TODO Auto-generated method stub
+		return shopDao.shopMain(shopnum);
+	}
+	@Override
+	public List<Map<String, Object>> getFoodOption(Map<String, Object> paramMap) {
+		// TODO Auto-generated method stub
+		return shopDao.getFoodOption(paramMap);
+	}
 }
