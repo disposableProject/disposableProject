ba860aa5febba903b4a5f5001cf614087f631d59
sonhoil <48711247+sonhoil@users.noreply.github.com>
2021-08-07 13:47:33 +0900

    0807 파일전달

--- DisposableProject/src/main/java/com/disposable/shop/controller/shopController.java
+++ DisposableProject/src/main/java/com/disposable/shop/controller/shopController.java
@@ -32,6 +32,7 @@ public class shopController {
 		System.out.println("resultMap>>>>>>>>>>>>>>>>>>>>" + resultMap2);
 		model.addAttribute("StoreFoodList", resultMap);
 		model.addAttribute("getStoreInfo", resultMap2);
+		
 		System.out.println("device ==>"+device);
 		if(device.equals("mobile")) {
 			System.out.println("check1");
@@ -53,29 +54,33 @@ public class shopController {
 		model.addAttribute("getStoreInfo", resultMap2);
 		model.addAttribute("getFoodInfo", resultMap3);
 		return "mobile/shop/MobileOptionSelect";
+		
+		
 	}
-
 	@RequestMapping(value="orderPayment.do")
 	public String orderPayment(HttpSession session, Model model) {
 		Map<String, Object> userInfo = (Map<String, Object>) session.getAttribute("userInfo");
 		System.out.println("userInfo====>" + userInfo);
 		String email = String.valueOf(userInfo.get("EMAIL"));
 		System.out.println("email ===>" + email);
+		
+		String orderCode = (String)session.getAttribute("orderCode");
+		String foodnum = (String)session.getAttribute("foodnum");
+		String optionnum = (String)session.getAttribute("optionnum");
+		System.out.println("orderCode ===>" + orderCode);
+		System.out.println("foodnum ===>" + foodnum);
+
 		Map<String, Object> paramMap = new HashMap<String, Object>();
 		paramMap.put("email", email);
+		paramMap.put("orderCode", orderCode);
+		paramMap.put("foodnum", foodnum);
+		paramMap.put("optionnum", optionnum);
+
 		List<Map<String,Object>> orderPayment = shopservice.orderPayment(paramMap);
 		System.out.println("orderPayment ====> "+orderPayment);
 		model.addAttribute("orderPayment", orderPayment);
 		
 		return "shop/order";
 	}
-
-	@RequestMapping(value="foodCart.do")
-	public String foodCart(Model model,@RequestParam("shopNum") String shopnum) {
-		Map<String, Object> paramMap = new HashMap<String, Object>();
-		paramMap.put("storenum", shopnum);
-		List<Map<String,Object>> resultMap = shopservice.getStoreInfo(shopnum);
-		model.addAttribute("getStoreInfo", resultMap);
-		return "mobile/shop/foodCart";
-	}
+	
 }
